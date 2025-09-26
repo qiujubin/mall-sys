505847b83482e9687f2279cf8f17f821fbfa4057
bin <44926027+qiujubin@users.noreply.github.com>
2025-09-26 12:15:27 +0800

    Delete src directory

--- src/main/java/com/gec/dao/BrandMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gec.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gec.domain.entity.Brand;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BrandMapper extends BaseMapper<Brand> {
-}
